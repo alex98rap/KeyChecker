7a9d73e86017798e99f93bd51abca99d4af725ca
alex1rap <alex1rap@spaces.ru>
2017-06-03 21:41:04 +0300

    Создан файл настроек мини-фреймворка

---
+++ config/web.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: alex1rap
+ * Date: 03.06.2017
+ * Time: 21:36
+ */
+
+return [
+    'lang' => 'ru-RU',
+    'routing' => [
+        'errors' => [
+            'error404' => [
+                'class' => 'app\\controllers\\siteController',
+                'action' => 'error404'
+            ],
+        ],
+        'rules' => [
+            //
+        ]
+    ]
+];
