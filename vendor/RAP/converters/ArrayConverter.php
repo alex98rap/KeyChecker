258a164717d8731e6f9976ca4e9cebae1cd616c6
alex1rap <alex1rap@spaces.ru>
2017-06-03 20:27:12 +0300

    Исправлен конвертер массивов

---
+++ vendor/RAP/converters/ArrayConverter.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: alex1rap
+ * Date: 03.06.2017
+ * Time: 19:32
+ */
+
+namespace RAP\converters;
+
+class ArrayConverter
+{
+    protected $array;
+
+    /**
+     * Converter constructor.
+     * @param array $array
+     */
+    public function __construct($array = [])
+    {
+        $this->array = $array;
+    }
+
+    /**
+     * @param array $array
+     */
+    public function setArray($array)
+    {
+        $this->array = $array;
+    }
+
+    public function asJSON()
+    {
+        return json_encode($this->array);
+    }
+
+    public function asXML()
+    {
+        return $this->toXML($this->array);
+    }
+
+    protected function toXML($array = [])
+    {
+        $result = '';
+        foreach($array as $item => $value) {
+            if (is_numeric($item)) {
+                $item = 'item_' . $item;
+            }
+            $value = !is_array($value) ? $value : $this->toXML($value);
+            $result .= "<{$item}>{$value}</{$item}>\r\n";
+        }
+        return $result;
+    }
+}
